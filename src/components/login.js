f70796b5c40dcb7439c9f78b86b48abe750f4cc4
reiyiyi <necro.2048@gmail.com>
2023-09-09 23:08:01 +0900

    UI設計

--- src/components/login.js
+++ src/components/login.js
@@ -3,8 +3,11 @@ import { Link } from 'react-router-dom';
 import LoginRequest from '../api-access/login';
 
 const Login = () => {
+    const data = LoginRequest("regi", "regipass")
     return (
         <div>
+            <p>{data.user_id}</p>
+            <p>{data.password}</p>
             <p>ログイン成功時</p>
             <Link to='/'>TOPページへ </Link>
         </div>
--- src/components/top.js
+++ src/components/top.js
@@ -4,19 +4,47 @@ import Test from '../api-access/test';
 import GetCycleRequest from '../api-access/getCycle';
 
 const Top = () => {
+    var session = ''
+    if (document.cookie != '') {
+        var cookies = document.cookie.split('; ');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie_data = cookies[i].split('=');
+            if (cookie_data[0] == 'session') {
+                session = decodeURIComponent(cookie_data[1]);
+                break;
+            }
+        }
+    }
+    const data = GetCycleRequest(session)
     return (
         <div>
-            <p>ログインしていない場合</p>
-            <Link to='/login'>ログイン </Link>
-            <Link to='/signup'>新規登録 </Link>
-            <p>ログインしている場合</p>
-            <Link to='/cycle/create'>サイクルを作る </Link>
-            <Link to='/report/try'>時間を報告する </Link>
-            <Link to='/report/gohobi'>使ったご褒美を報告する </Link>
-            <p>APIテスト</p>
-            <p>{Test()}</p>
+            {'status' in data ?
+                (
+                    <div>
+                        <Link to='/login'>ログイン </Link>
+                        <Link to='/signup'>新規登録 </Link>
+                    </div>
+                )
+                :
+                (
+                    <div>
+                        {
+                            data.is_created == true ?
+                                (<div>
+                                    <p>{data.try}をあと{data.requiredTime}分頑張ったら{data.gohobi}GET!!</p>
+                                    <Link to='/cycle/create'>サイクルを作り直す</Link>
+                                    <Link to='/report/try'>がんばりを報告する</Link>
+                                    <Link to='/report/gohobi'>使ったごほうびを報告する</Link>
+                                </div>)
+                                :
+                                (<Link to='/cycle/create'>サイクルを作ってみる！ </Link>)
+                        }
+
+                    </div>
+                )
+            }
         </div>
     );
-  };
+};
 
 export default Top;
